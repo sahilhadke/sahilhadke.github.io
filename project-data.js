a66f9c8bb3c325d7607ac29aaa55872ec30e815d
Sahil Hadke <sahil.hadke@gmail.com>
2025-12-07 15:18:23 -0700

    added projects

--- project-data.js
+++ project-data.js
@@ -16,5 +16,35 @@ const projectsData = [
         description: "We developed a centralized CRM for Habitat for Humanity to streamline data management and operations. Featuring dynamic dashboards and an AI chatbot, our solution amplifies community impact through real-time insights.",
         link: "https://devpost.com/software/the-vizards",
         image: "./img/projects/14.png"
+    },
+    {
+        title: "Melodia",
+        description: "Melodia.ai automates the complex process of adding custom beats to existing songs. Using Python and Essentia for seamless synchronization, this tool allows users to blend new instruments with original tracks. Awarded 2nd place at Sunhacks 2024.",
+        link: "https://devpost.com/software/melodia-6cqsn4",
+        image: "./img/projects/7.png"
+    },
+    {
+        title: "Customer Relationship Management",
+        description: "A modular CRM system designed for schools and organizations in India to streamline data and operations. We developed customizable features for lead management and analytics, helping 4 organizations manage over 1000+ leads collectively. Published in IJARSCT.",
+        link: "https://ijarsct.co.in/Paper7119.pdf",
+        image: "./img/projects/3.png"
+    },
+    {
+        title: "JobTailor",
+        description: "A Python package that leverages the Gemini LLM to automate resume and cover letter customization. By analyzing a master resume and specific job descriptions, it generates tailored documents to increase interview chances.",
+        link: "https://github.com/sahilhadke/job-tailor",
+        image: "./img/projects/6.png"
+    },
+    {
+        title: "RAG-based Recursive Web Crawler",
+        description: "A powerful web application combining Retrieval-Augmented Generation (RAG) with a recursive web crawler. It extracts data from entire websites to create an interactive knowledge base, allowing users to query content using advanced language models.",
+        link: "https://github.com/sahilhadke/website-rag",
+        image: "./img/projects/10.png"
+    },
+    {
+        title: "Dijkstra's Algorithm Visualization",
+        description: "A real-time interactive visualization of Dijkstra's Algorithm built in Unity. It stands as a personal milestone: coding complex graph problems from scratch at 19 years old without relying on any external AI tools.",
+        link: "https://sahil13.itch.io/dj-algo",
+        image: "./img/projects/9.png"
     }
 ];
